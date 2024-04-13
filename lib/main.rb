51c31b2cce48fde30a6adb95eba40501987693a2
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-04-12 21:49:53 -0300

    feat: add block of code to get resenha server

--- lib/main.rb
+++ lib/main.rb
@@ -7,8 +7,12 @@ Dotenv.load
 
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 
-bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
+bot.ready do
+  RESENHA_SERVER_NAME = 'A Resenha'
+  resenha = nil
+  bot.servers.each do |id, server|
+    resenha = server if server.name == RESENHA_SERVER_NAME
+  end
 end
 
 bot.run
